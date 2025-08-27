e18e499e6a57118d5d1e0632bc3df28c778870a6
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-08-27 15:10:52 +0900

    feat : add chip list component

---
+++ my-app/src/components/chip-list.js
@@ -0,0 +1,20 @@
+import css from "./chip-list.module.css";
+
+function Chip({ incomingChipList }) {
+  return (
+    <>
+      <>
+        {incomingChipList != null
+          ? incomingChipList.map((value, index) => (
+              <span className={css["chip-object"]} contentEditable="false">
+                {" "}
+                {value}
+              </span>
+            ))
+          : ""}
+      </>
+    </>
+  );
+}
+
+export default Chip;
