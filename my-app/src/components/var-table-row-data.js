fa44b3873b3e1d553e27654c3d3ee6fb7fc80fe3
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-05 13:36:03 +0900

    feat : Added new way to insert a Variable Table Row Data

---
+++ my-app/src/components/var-table-row-data.js
@@ -0,0 +1,108 @@
+import { useRef } from "react";
+
+function VarTableRowData({
+  incomingKey,
+  incomingValues,
+  tableDataType,
+  incomingchangedValues,
+  incomingHandleVariableChanges,
+}) {
+  const editableRef = useRef();
+
+  const TableRowFormatRef = {
+    VarName: { type: "text", VarRef: "name" },
+    VarStartValue: {
+      type: () => {
+        switch (incomingValues.type) {
+          case "Integer":
+            return "number";
+          case "Date":
+            return "date";
+          default:
+            return "text";
+        }
+      },
+      VarRef: "value",
+    },
+  };
+
+  const selectedTableData = () => {
+    console.log(`TableRowFormatRef: `, TableRowFormatRef);
+    for (const key in TableRowFormatRef) {
+      console.log(
+        `TableRowFormatRefKey: `,
+        TableRowFormatRef[key],
+        "Key:",
+        key
+      );
+      if (tableDataType == key) {
+        console.log(`TableRowFormatRefKeyIf: `, TableRowFormatRef[key]);
+        return TableRowFormatRef[key];
+      }
+    }
+  };
+
+  function getTableRowDataAttribute(attributeType, eventCall) {
+    let valueType = incomingValues.type;
+    let result = null;
+    switch (attributeType) {
+      case "type":
+        result = selectedTableData();
+        return result?.type();
+      case "value":
+        switch (valueType) {
+          case "Integer":
+            return incomingValues.value;
+          case "Date":
+            return incomingValues.dateValue
+              ? incomingValues.dateValue.toISOString().split("T")[0]
+              : "";
+          default:
+            break;
+        }
+        break;
+      case "onChange":
+        switch (valueType) {
+          case "Integer":
+            result = () => {
+              return incomingHandleVariableChanges(
+                incomingKey,
+                incomingchangedValues,
+                eventCall.target.value
+              );
+            };
+            return result();
+          case "Date":
+            result = () => {
+              const selectedDate = eventCall.target.value
+                ? new Date(eventCall.target.value)
+                : null;
+
+              return incomingHandleVariableChanges(
+                incomingKey,
+                incomingchangedValues,
+                selectedDate
+              );
+            };
+            return result();
+          default:
+            break;
+        }
+        break;
+      default:
+        return null;
+    }
+  }
+  return (
+    <>
+      <input
+        ref={editableRef}
+        type={getTableRowDataAttribute("type", null)}
+        value={getTableRowDataAttribute("value", null)}
+        onChange={(e) => getTableRowDataAttribute("onChange", e)}
+      ></input>
+    </>
+  );
+}
+
+export default VarTableRowData;
