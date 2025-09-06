86107dd8ca5d25e2b2bcd483e64b007d28ac0fb4
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-07 00:12:01 +0900

    feat : Clean Old Table Row Data Codes

--- my-app/src/components/var-table-body.js
+++ my-app/src/components/var-table-body.js
@@ -14,15 +14,6 @@ function VarTableBody({
       <tbody className={css["tbody-name"]}>
         {Array.from(incomingVariablesBody.entries()).map(([key, values]) => (
           <tr key={key}>
-            {/* <td
-              contentEditable="true"
-              onBlur={(e) =>
-                incomingHandleVariableChanges(key, "name", e.target.innerText)
-              }
-              className={css["td-var-name"]}
-            >
-              {values.name}
-            </td> */}
             <td>
               <VarTableRowData
                 incomingKey={key}
@@ -78,19 +69,6 @@ function VarTableBody({
                 <>
                   {values.type === "Integer" ? (
                     <>
-                      {/* <td
-                        contentEditable="true"
-                        onBlur={(e) =>
-                          incomingHandleVariableChanges(
-                            key,
-                            "minValue",
-                            e.target.innerText
-                          )
-                        }
-                        className={css["td-var-value"]}
-                      >
-                        {values.minValue}
-                      </td> */}
                       <td>
                         <VarTableRowData
                           incomingKey={key}
@@ -102,19 +80,7 @@ function VarTableBody({
                           }
                         ></VarTableRowData>
                       </td>
-                      {/* <td
-                        contentEditable="true"
-                        onBlur={(e) =>
-                          incomingHandleVariableChanges(
-                            key,
-                            "maxValue",
-                            e.target.innerText
-                          )
-                        }
-                        className={css["td-var-value"]}
-                      >
-                        {values.maxValue}
-                      </td> */}
+
                       <td>
                         <VarTableRowData
                           incomingKey={key}
@@ -130,25 +96,6 @@ function VarTableBody({
                   ) : (
                     <>
                       <td>
-                        {/* <input
-                          type="date"
-                          value={
-                            values.minDateValue
-                              ? values.minDateValue.toISOString().split("T")[0]
-                              : ""
-                          }
-                          onChange={(e) => {
-                            const selectedDate = e.target.value
-                              ? new Date(e.target.value)
-                              : null;
-                            incomingHandleVariableChanges(
-                              key,
-                              "minDateValue",
-                              selectedDate
-                            );
-                          }}
-                          className={css["td-var-value"]}
-                        /> */}
                         <VarTableRowData
                           incomingKey={key}
                           incomingValues={values}
@@ -160,25 +107,6 @@ function VarTableBody({
                         ></VarTableRowData>
                       </td>
                       <td>
-                        {/* <input
-                          type="date"
-                          value={
-                            values.maxDateValue
-                              ? values.maxDateValue.toISOString().split("T")[0]
-                              : ""
-                          }
-                          onChange={(e) => {
-                            const selectedDate = e.target.value
-                              ? new Date(e.target.value)
-                              : null;
-                            incomingHandleVariableChanges(
-                              key,
-                              "maxDateValue",
-                              selectedDate
-                            );
-                          }}
-                          className={css["td-var-value"]}
-                        /> */}
                         <VarTableRowData
                           incomingKey={key}
                           incomingValues={values}
@@ -209,19 +137,6 @@ function VarTableBody({
 
                     {values.type !== "Date" ? (
                       <>
-                        {/* <input
-                          ref={editableRef}
-                          type="text"
-                          value={values.value}
-                          onChange={(e) =>
-                            incomingHandleVariableChanges(
-                              key,
-                              "value",
-                              e.target.value
-                            )
-                          }
-                          className={css["td-var-value"]}
-                        ></input> */}
                         <VarTableRowData
                           incomingKey={key}
                           incomingValues={values}
@@ -234,25 +149,6 @@ function VarTableBody({
                       </>
                     ) : (
                       <>
-                        {/* <input
-                          type="date"
-                          value={
-                            values.dateValue
-                              ? values.dateValue.toISOString().split("T")[0]
-                              : ""
-                          }
-                          onChange={(e) => {
-                            const selectedDate = e.target.value
-                              ? new Date(e.target.value)
-                              : null;
-                            incomingHandleVariableChanges(
-                              key,
-                              "dateValue",
-                              selectedDate
-                            );
-                          }}
-                          className={css["td-var-value"]}
-                        /> */}
                         <VarTableRowData
                           incomingKey={key}
                           incomingValues={values}
@@ -275,22 +171,8 @@ function VarTableBody({
               values.type === "List" ||
               values.type === "Date" ? (
                 <td
-                  // contentEditable="true"
                   className={css["td-var-iterate"]}
                 >
-                  {/* <div>
-                    <input
-                      type="checkbox"
-                      checked={values.iterate}
-                      onClick={() =>
-                        incomingHandleVariableChanges(
-                          key,
-                          "iterate",
-                          !values.iterate
-                        )
-                      }
-                    />
-                  </div> */}
                   <VarTableRowData
                     incomingKey={key}
                     incomingValues={values}
@@ -310,18 +192,6 @@ function VarTableBody({
               values.type === "List" ||
               values.type === "Date" ? (
                 <>
-                  {/* <td
-                    contentEditable="true"
-                    onBlur={(e) =>
-                      incomingHandleVariableChanges(
-                        key,
-                        "interval",
-                        parseInt(e.target.innerText)
-                      )
-                    }
-                  >
-                    {values.interval}
-                  </td> */}
                   <td>
                     <VarTableRowData
                       incomingKey={key}
@@ -343,21 +213,6 @@ function VarTableBody({
               values.type === "List" ||
               values.type === "Date" ? (
                 <td>
-                  {/* <div>
-                    <label>
-                      <input
-                        type="checkbox"
-                        checked={values.randomize}
-                        onClick={() =>
-                          incomingHandleVariableChanges(
-                            key,
-                            "randomize",
-                            !values.randomize
-                          )
-                        }
-                      />
-                    </label>
-                  </div> */}
                   <VarTableRowData
                     incomingKey={key}
                     incomingValues={values}
