7641746e10b4cd512031c2436779e9b2139d2848
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-11 21:20:20 +0900

    feat : Added New Modal Functionality and Components

---
+++ my-app/src/components/var-modal.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import VarTableRowData from "./var-table-row-data";
+
+function VariableModal({
+  modalState,
+  setModalState,
+  incomingIndex,
+  incomingValues,
+  incomingHandleVariableChanges,
+}) {
+  function changeModalCondition() {
+    console.log(`MODAL key :${incomingIndex}, values: ${incomingValues}`);
+    console.log("Modal:", incomingIndex, incomingValues);
+    setModalState(!modalState);
+  }
+
+  if (!incomingValues) {
+    return null;
+  }
+
+  return (
+    <>
+      {modalState ? (
+        <>
+          <div className="w-screen h-screen inset-0 fixed z-[100]">
+            <div className="w-screen h-screen inset-0 fixed bg-[rgba(49,49,49,0.8)]">
+              <div className="absolute -translate-x-2/4 -translate-y-2/4 leading-[1.4] max-w-[600px] min-w-[300px] px-7 py-3.5 rounded-[3px] left-2/4 top-2/4 bg-main-color">
+                <h1>
+                  <VarTableRowData
+                    incomingKey={incomingIndex}
+                    incomingValues={incomingValues}
+                    tableDataType={"VarName"}
+                    incomingchangedValues={"name"}
+                    incomingHandleVariableChanges={
+                      incomingHandleVariableChanges
+                    }
+                    incomingCustomColorText="opposite-color"
+                  ></VarTableRowData>
+                </h1>
+                <div>
+                  <div className="w-full h-full grid place-items-center grid-cols-2">
+                    <div>
+                      Iterate
+                      <VarTableRowData
+                        incomingKey={incomingIndex}
+                        incomingValues={incomingValues}
+                        tableDataType={"VarBoolean"}
+                        incomingchangedValues={"iterate"}
+                        incomingHandleVariableChanges={
+                          incomingHandleVariableChanges
+                        }
+                      ></VarTableRowData>
+                    </div>
+                    <div>
+                      Randomize
+                      <VarTableRowData
+                        incomingKey={incomingIndex}
+                        incomingValues={incomingValues}
+                        tableDataType={"VarBoolean"}
+                        incomingchangedValues={"randomize"}
+                        incomingHandleVariableChanges={
+                          incomingHandleVariableChanges
+                        }
+                      ></VarTableRowData>
+                    </div>
+                    <div>
+                      <h2>
+                        Start Value
+                       <VarTableRowData
+                          incomingKey={incomingIndex}
+                          incomingValues={incomingValues}
+                          tableDataType={"VarStartValue"}
+                          incomingchangedValues={"value"}
+                          incomingHandleVariableChanges={
+                            incomingHandleVariableChanges
+                          }
+                        ></VarTableRowData>
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+                <button
+                  onClick={(e) => changeModalCondition()}
+                  className="bg-opposite-color border-2 border-main-color p-[10px] hover:bg-main-color hover:border-opposite-color"
+                >
+                  Close button
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+export default VariableModal;
