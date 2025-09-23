e39ca75b660b825a3539a222f979a72a2b4ba052
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-23 18:10:22 +0900

    feat : Added Custom Toggle

---
+++ my-app/src/components/toggle-custom.js
@@ -0,0 +1,18 @@
+function CustomToggle({ onChange = null, checked = null, onClick = null }) {
+  return (
+    <>
+      <label className="relative inline-flex items-center cursor-pointer">
+        <input
+          type="checkbox"
+          onChange={onChange}
+          className="sr-only peer"
+          onClick={onClick}
+          checked={checked}
+        />
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-amber-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-amber-600"></div>
+      </label>
+    </>
+  );
+}
+
+export default CustomToggle;
