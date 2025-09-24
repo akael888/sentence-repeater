0e312f10945ac108df43287a90bb26f165aade1d
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-24 15:29:07 +0900

    backdrop-blur-[6px] backdrop-saturate-[100%]

--- my-app/src/components/burger.js
+++ my-app/src/components/burger.js
@@ -2,25 +2,38 @@ import { motion } from "motion/react";
 
 function Burger({ isOpen, handleOpenBurgerMenuChanges }) {
   return (
-    <>
-      <motion.button
-        onClick={handleOpenBurgerMenuChanges}
-        className="w-8 h-8 flex justify-center items-center relative [&>*]:bg-white z-[1000]"
-      >
-        <motion.span
-          className="absolute  block w-8 h-1 rounded-[5px]"
-          animate={isOpen ? { rotate: 45,y:0} : { rotate: 0, y: -8 }}
-        ></motion.span>
-        <motion.span
-          className="absolute  block w-8 h-1 rounded-[5px]"
-          animate={isOpen ? { opacity: 0 } : { opacity: 1 }}
-        ></motion.span>
-        <motion.span
-          className="absolute  block w-8 h-1 rounded-[5px]"
-          animate={isOpen ? { rotate: -45, y:0} : { rotate: 0, y: 8 }}
-        ></motion.span>
-      </motion.button>
-    </>
+    <motion.button
+      onClick={handleOpenBurgerMenuChanges}
+      className="w-8 h-8 flex justify-center items-center relative [&>*]:bg-white z-[1000]"
+    >
+      <motion.span
+        className="absolute block w-8 h-1 rounded-[5px]"
+        animate={
+          isOpen
+            ? { rotate: 45, y: 0, backgroundColor: "black" }
+            : { rotate: 0, y: -8, backgroundColor: "white" }
+        }
+        transition={{ duration: 0.3 }}
+      />
+      <motion.span
+        className="absolute block w-8 h-1 rounded-[5px]"
+        animate={
+          isOpen
+            ? { opacity: 0, backgroundColor: "black" }
+            : { opacity: 1, backgroundColor: "white" }
+        }
+        transition={{ duration: 0.3 }}
+      />
+      <motion.span
+        className="absolute block w-8 h-1 rounded-[5px]"
+        animate={
+          isOpen
+            ? { rotate: -45, y: 0, backgroundColor: "black" }
+            : { rotate: 0, y: 8, backgroundColor: "white" }
+        }
+        transition={{ duration: 0.3 }}
+      />
+    </motion.button>
   );
 }
 
