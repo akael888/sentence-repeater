cd4725034a104987b43c3f22ed2ff567303d3623
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-24 15:31:05 +0900

    feat : Added Value Functionality in the Custom Toggle

--- my-app/src/components/toggle-custom.js
+++ my-app/src/components/toggle-custom.js
@@ -3,6 +3,7 @@ function CustomToggle({
   checked = null,
   onClick = null,
   disabled = null,
+  value = null,
 }) {
   return (
     <>
@@ -14,6 +15,7 @@ function CustomToggle({
           onClick={onClick}
           checked={checked}
           disabled={disabled}
+          value={value}
         />
         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-amber-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-amber-600"></div>
       </label>
