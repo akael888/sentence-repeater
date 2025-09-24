7c973804755b769cd6baedc97906a261077449c4
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-24 15:27:18 +0900

    feat : Added Wave Background and Dark Mode Capabilities

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,17 +6,30 @@ import bg from "./img/background-img.jpg";
 import { motion } from "motion/react";
 import Burger from "./components/burger";
 import BurgerMenu from "./components/burger-menu";
+import WaveBackground from "./components/blob-background";
 
 function App() {
   // Repeater Components
 
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    try {
+      const stored = localStorage.getItem("IS_DARK_MODE");
+      if (stored) {
+        const parsed = JSON.parse(stored);
+        return parsed;
+      }
+    } catch (err) {
+      console.error("Failed to parse Dark Mode from localStorage:", err);
+    }
+    return false;
+  });
   const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
   let tw_appHeader_glassMorphBG =
-    " bg-[color-mix(in_srgb,var(--color-foreground)_20%,transparent)] backdrop-blur-[10px]";
+    "  backdrop-blur-[6px] backdrop-saturate-[100%] ";
 
   const handleDarkModeChanges = (event) => {
     setIsDarkMode(!isDarkMode);
+
     console.log("Current Mode is : " + isDarkMode);
   };
 
@@ -25,6 +38,7 @@ function App() {
   };
 
   useEffect(() => {
+    localStorage.setItem("IS_DARK_MODE", JSON.stringify(isDarkMode));
     if (isDarkMode === true) {
       document.body.classList.add("dark");
     } else {
@@ -33,86 +47,99 @@ function App() {
   }, [isDarkMode]);
 
   return (
-    <div
-      className="w-screen h-screen bg-center bg-repeat text-center flex flex-col"
-      style={{ backgroundImage: `url(${bg})` }}
-    >
-      <BurgerMenu isOpen={isOpenBurgerMenu} darkModeChanges={handleDarkModeChanges} ></BurgerMenu>
-      <motion.header
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 0.5,
-          ease: "easeInOut",
-        }}
-        className={
-          "h-[5%] min-h-[5vh] w-full flex flex-col items-center justify-center font-[calc(10px_+_2vmin)] text-main-color text-center z-[100] sticky top-0" +
-          tw_appHeader_glassMorphBG
-        }
-        
+    <>
+      <WaveBackground isDarkMode={isDarkMode}></WaveBackground>
+      <div
+        className="w-screen h-screen bg-center bg-cover text-center flex flex-col"
+        style={{ color: isDarkMode ? "white" : "grey" }}
+        // style={{ backgroundImage: `url(${bg})` }}
       >
-        <TitleHeader
-          darkModeTitle={isDarkMode}
-          darkModeChangesTitle={handleDarkModeChanges}
+        <BurgerMenu
           isOpen={isOpenBurgerMenu}
-          handleOpenBurgerMenuChanges = {handleOpenBurgerMenuChanges}
-        />
-      </motion.header>
+          darkModeChanges={handleDarkModeChanges}
+          isDarkMode={isDarkMode}
+        ></BurgerMenu>
+        <motion.header
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+          className={
+            "h-[5%] min-h-[5vh] w-full flex flex-col items-center justify-center font-[calc(10px_+_2vmin)] text-main-color text-center z-[100] sticky top-0" +
+            tw_appHeader_glassMorphBG
+          }
+        >
+          <TitleHeader
+            darkModeTitle={isDarkMode}
+            darkModeChangesTitle={handleDarkModeChanges}
+            isOpen={isOpenBurgerMenu}
+            handleOpenBurgerMenuChanges={handleOpenBurgerMenuChanges}
+          />
+        </motion.header>
 
-      <div className="flex-1 overflow-y-auto overflow-x-hidden">
-        <div className="min-h-full flex flex-col">
-          <div className="flex-1 w-full grid place-items-center p-[5%]">
-            <Repeater />
-          </div>
+        <div className="flex-1 overflow-y-auto overflow-x-hidden">
+          <div className="min-h-full flex flex-col">
+            <div className="flex-1 w-full grid place-items-center p-[5%] z-[98]">
+              <Repeater />
+            </div>
 
-          <motion.footer
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{
-              duration: 0.5,
-              ease: "easeInOut",
-            }}
-            className="w-full h-auto sm:h-[50%] bg-color-foreground mt-auto sm:p-[0] p-[2%]"
-          >
-            <div className="w-full h-auto sm:h-[50%] flex flex-col sm:flex-row p-[2%] sm:p-[1%] ">
-              <div className="w-full grid place-items-center sm:flex sm:flex-row sm:items-center sm:justify-center sm:gap-[10px] text-sub-color">
-                <p className="text-sm">
-                  Build with React, Tailwindcss, React Bootstrap, and Framer Motion
-                </p>
-              </div>
-              <div className="w-full grid place-items-center sm:flex sm:flex-row sm:items-center sm:justify-center sm:gap-[10px] text-main-color">
-                <p className="text-sm ">© 2025 - Elgratio Latuihamallo</p>
-                <div className="w-fit h-fit sm:w-auto flex gap-[10px]">
-                  <motion.a
-                    href="https://www.linkedin.com/in/elgratiofc"
-                    whileTap={{ scale: 0.9 }}
-                    className="text-opposite-color no-underline hover:text-opposite-sub-color h-fit"
-                  >
-                    <strong>
-                      <p className="text-sm">Linkedin</p>
-                    </strong>
-                  </motion.a>
-                  <p className="text-sm">|</p>
-                  <motion.a
-                    href="https://github.com/akael888"
-                    whileTap={{ scale: 0.9 }}
-                    className="text-opposite-color no-underline hover:text-opposite-sub-color h-fit"
-                  >
-                    <strong>
-                      <p className="text-sm">Github</p>
-                    </strong>
-                  </motion.a>
+            <motion.footer
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className="w-full h-auto sm:h-[50%] bg-transparent mt-auto sm:p-[0] p-[2%] z-[98]"
+            >
+              <div className="w-full h-auto sm:h-[50%] flex flex-col sm:flex-row p-[2%] sm:p-[1%] ">
+                <div className="w-full grid place-items-center text-opposite-color sm:flex sm:flex-row sm:items-center sm:justify-center sm:gap-[10px] ">
+                  <p className="text-sm">
+                    Build with React, Tailwindcss, React Bootstrap, and Framer
+                    Motion
+                  </p>
+                </div>
+                <div className="w-full grid place-items-center sm:flex sm:flex-row sm:items-center sm:justify-center sm:gap-[10px] text-white ">
+                  <p className="text-sm ">© 2025 - Elgratio Latuihamallo</p>
+                  <div className="w-fit h-fit sm:w-auto flex gap-[10px]">
+                    <motion.a
+                      href="https://www.linkedin.com/in/elgratiofc"
+                      whileTap={{ scale: 0.9 }}
+                      className="text-opposite-color no-underline hover:text-opposite-sub-color h-fit"
+                    >
+                      <strong>
+                        <p className="text-sm">Linkedin</p>
+                      </strong>
+                    </motion.a>
+                    <p className="text-sm">|</p>
+                    <motion.a
+                      href="https://github.com/akael888"
+                      whileTap={{ scale: 0.9 }}
+                      className="text-opposite-color no-underline hover:text-opposite-sub-color h-fit"
+                    >
+                      <strong>
+                        <p className="text-sm">Github</p>
+                      </strong>
+                    </motion.a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </motion.footer>
+            </motion.footer>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-function TitleHeader({ darkModeTitle, darkModeChangesTitle, isOpen,handleOpenBurgerMenuChanges }) {
+function TitleHeader({
+  darkModeTitle,
+  darkModeChangesTitle,
+  isOpen,
+  handleOpenBurgerMenuChanges,
+}) {
   return (
     <>
       <nav className="w-full h-full flex justify-end items-center p-[10px]">
@@ -123,7 +150,10 @@ function TitleHeader({ darkModeTitle, darkModeChangesTitle, isOpen,handleOpenBur
             alt="sentence-repeater-logo"
           />
         </a> */}
-        <Burger isOpen={isOpen} handleOpenBurgerMenuChanges={handleOpenBurgerMenuChanges}></Burger>
+        <Burger
+          isOpen={isOpen}
+          handleOpenBurgerMenuChanges={handleOpenBurgerMenuChanges}
+        ></Burger>
         {/* <Mode
           currentState={darkModeTitle}
           darkModeChanges={darkModeChangesTitle}
