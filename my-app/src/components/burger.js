714496a843aa8a9fc44dc8a1656383f4399ea266
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-25 14:50:19 +0900

    fix : Remove Change Color when Animation on Burger Button and Set Color for Dark Mode/Light Mode

--- my-app/src/components/burger.js
+++ my-app/src/components/burger.js
@@ -4,33 +4,21 @@ function Burger({ isOpen, handleOpenBurgerMenuChanges }) {
   return (
     <motion.button
       onClick={handleOpenBurgerMenuChanges}
-      className="w-8 h-8 flex justify-center items-center relative [&>*]:bg-white z-[1000]"
+      className="w-8 h-8 flex justify-center items-center relative [&>*]:bg-stone-500 dark:[&>*]:bg-white z-[1000]"
     >
       <motion.span
         className="absolute block w-8 h-1 rounded-[5px]"
-        animate={
-          isOpen
-            ? { rotate: 45, y: 0, backgroundColor: "black" }
-            : { rotate: 0, y: -8, backgroundColor: "white" }
-        }
+        animate={isOpen ? { rotate: 45, y: 0 } : { rotate: 0, y: -8 }}
         transition={{ duration: 0.3 }}
       />
       <motion.span
         className="absolute block w-8 h-1 rounded-[5px]"
-        animate={
-          isOpen
-            ? { opacity: 0, backgroundColor: "black" }
-            : { opacity: 1, backgroundColor: "white" }
-        }
+        animate={isOpen ? { opacity: 0 } : { opacity: 1 }}
         transition={{ duration: 0.3 }}
       />
       <motion.span
         className="absolute block w-8 h-1 rounded-[5px]"
-        animate={
-          isOpen
-            ? { rotate: -45, y: 0, backgroundColor: "black" }
-            : { rotate: 0, y: 8, backgroundColor: "white" }
-        }
+        animate={isOpen ? { rotate: -45, y: 0 } : { rotate: 0, y: 8 }}
         transition={{ duration: 0.3 }}
       />
     </motion.button>
