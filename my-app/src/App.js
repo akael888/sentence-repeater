749138708b76f1c63be7bd0200530d0b9d060b66
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-09-25 13:58:54 +0900

    feat: Added Border to be Controlled by Dark Mode As Well

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -62,7 +62,7 @@ function App() {
     <>
       <WaveBackground isDarkMode={isDarkMode}></WaveBackground>
       <div
-        className="w-screen h-screen bg-center bg-cover text-center flex flex-col [&>*]:text-stone-500 dark:[&>*]:!text-white"
+        className="w-screen h-screen bg-center bg-cover text-center flex flex-col [&>*]:text-stone-500 [&>*]:border-stone-500 dark:[&>*]:!text-white dark:[&>*]:border-white "
         // style={{ backgroundImage: `url(${bg})` }}
       >
         {/* <div className="p-10 bg-white text-black dark:bg-black dark:text-white z-[100000]">
