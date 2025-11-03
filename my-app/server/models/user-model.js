d33c63fe63154722a3e96a2435317705946fef2f
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-03 17:42:02 +0900

    feat : Added Database Scheme and Model

---
+++ my-app/server/models/user-model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide username"],
+    minlength: 3,
+    maxlength: 10,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
