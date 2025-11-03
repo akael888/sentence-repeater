97dbf85e2e24049bb351ebe9e05a3bb53d87447b
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-03 17:43:46 +0900

    feat : Connect the Auth Controller to the User Model

--- my-app/server/controller/auth.js
+++ my-app/server/controller/auth.js
@@ -1,7 +1,11 @@
 const { StatusCodes } = require("http-status-codes");
+const User = require("../models/user-model");
 
-const register = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "Register" });
+const register = async (req, res) => {
+  // const user = req.body;
+  // console.log(user);
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.OK).json({ status: "connected", user: user });
 };
 const login = (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "login" });
