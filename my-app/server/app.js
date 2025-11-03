2d5c154820e6e122874101193e4d4754dbcc0361
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-03 17:44:28 +0900

    feat : Added Database Connection and other middlewares

--- my-app/server/app.js
+++ my-app/server/app.js
@@ -1,14 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./db/connect");
 const app = express();
 const authRouter = require("./routes/auth");
 const sentenceRouter = require("./routes/sentence");
+
+const notFoundMiddleware = require("./middleware/not-found");
+
 const port = process.env.PORT || 8000;
+
+app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/sentence", sentenceRouter);
 
+//middleware
+app.use(notFoundMiddleware);
+
 const start = async () => {
   try {
+    console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`app is listening to port: ${port}`);
     });
