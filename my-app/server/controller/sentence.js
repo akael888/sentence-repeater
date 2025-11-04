cce2c67f837de105ad25b95ee9bd71ab4c7ec196
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-04 16:21:42 +0900

    feat : Added Sentence Functionality in Controller

--- my-app/server/controller/sentence.js
+++ my-app/server/controller/sentence.js
@@ -1,26 +1,76 @@
 const { StatusCodes } = require("http-status-codes");
+const Sentence = require("../models/sentence-model");
+const {
+  CustomAPIError,
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} = require("../errors");
 
 const getAllSentence = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "getAllSentence" });
+  const sentence = await Sentence.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+
+  if (!sentence) {
+    throw new NotFoundError("No Sentence Found");
+  }
+  res.status(StatusCodes.OK).json({ sentence, sentenceCount: sentence.length });
 };
 
 const getSentence = async (req, res) => {
-  const { id } = req.params;
-  res.status(StatusCodes.OK).json({ msg: `getSentence ${id}` });
+  const sentence = await Sentence.find({
+    createdBy: req.user.userId,
+    _id: req.params.id,
+  }).sort("createdAt");
+
+  if (!sentence) {
+    throw new NotFoundError(`No Sentence Found with this ID: ${id}`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `Found this ${req.params.id} sentence`, sentence });
 };
 
 const createSentence = async (req, res) => {
-  const { id } = req.params;
-  res.status(StatusCodes.OK).json({ msg: `createSentence ${id}` });
+  req.body.createdBy = req.user.userId;
+  const sentence = await Sentence.create(req.body);
+  res.status(StatusCodes.OK).json({ msg: `New Sentence Created!`, sentence });
 };
 
 const editSentence = async (req, res) => {
-  const { id } = req.params;
-  res.status(StatusCodes.OK).json({ msg: `editSentence ${id}` });
+  const sentence = await Sentence.findOneAndUpdate(
+    {
+      createdBy: req.user.userId,
+      _id: req.params.id,
+    },
+    req.body,
+    { new: true }
+  );
+
+   if (!sentence) {
+    throw new NotFoundError(`No Sentence Found with this ID: ${id}`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `${req.params.id} Sentence Edited!`, sentence });
 };
 
 const deleteSentece = async (req, res) => {
-  const { id } = req.params;
+  const sentence = await Sentence.findOneAndRemove(
+    {
+      createdBy: req.user.userId,
+      _id: req.params.id,
+    },
+    query
+  );
+
+   if (!sentence) {
+    throw new NotFoundError(`No Sentence Found with this ID: ${id}`);
+  }
+
   res.status(StatusCodes.OK).json({ msg: `deleteSentece ${id}` });
 };
 
