6f6e69ebbd23d5954ece1d6cba2a0f483ef216fd
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-17 23:30:16 +0900

    chore : Adjusted Old Form for Auth and Created a new Form for Auth As Well

--- my-app/client/src/components/login-form.js
+++ my-app/client/src/components/login-form.js
@@ -1,64 +1,8 @@
-import { useState } from "react";
-
 function Login({ currentLink }) {
-  const [formData, setFormData] = useState({ username: "", password: "" });
-  const [message, setMessage] = useState("");
-  const link = currentLink;
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await fetch(`${link}/api/v1/auth/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-        credentials:'include'
-      });
-
-      const data = await res.json();
-
-      console.log("this is Data");
-      console.log(data.object);
-
-      if (res.ok) {
-        setMessage(data.msg);
-      } else {
-        setMessage(`Login Failed : ${data.msg}`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
-      <div className="border-1">
-        <h3>Login Debugger</h3>
-        <p>{message}</p>
-        <form onSubmit={handleSubmit} className="text-black">
-          <input
-            name="username"
-            placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-          ></input>
-          <input
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-          ></input>
-          <button type="submit">Login</button>
-        </form>
-      </div>
+      <button className="border-1 rounded-1 p-1 hover:bg-white hover:text-black" >Login</button>
     </>
   );
 }
-
 export default Login;
---
+++ my-app/client/src/components/register-form.js
@@ -0,0 +1,8 @@
+function Register({ currentLink }) {
+  return (
+    <>
+      <button className="border-1 rounded-1 p-1 hover:bg-white hover:text-black" >Register</button>
+    </>
+  );
+}
+export default Register;
