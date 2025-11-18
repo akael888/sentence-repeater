14b43e15d4d19753f4e90ce684cf8647b2337386
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-18 22:24:27 +0900

    feat : UI Adjustments

--- my-app/client/src/components/sentence-list.js
+++ my-app/client/src/components/sentence-list.js
@@ -14,6 +14,42 @@ function SentenceList({ currentSentence }) {
       sentence: "ABC{}",
       isOptionOpened: false,
     },
+    2: {
+      name: "Test 3",
+      description: "ABC",
+      sentence: "ABC{}",
+      isOptionOpened: false,
+    },
+    3: {
+      name: "Test 4",
+      description: "ABC",
+      sentence: "ABC{}",
+      isOptionOpened: false,
+    },
+    4: {
+      name: "Test 4",
+      description: "ABC",
+      sentence: "ABC{}",
+      isOptionOpened: false,
+    },
+    5: {
+      name: "Test 5",
+      description: "ABC",
+      sentence: "ABC{}",
+      isOptionOpened: false,
+    },
+    6: {
+      name: "Test 6",
+      description: "ABC",
+      sentence: "ABC{}",
+      isOptionOpened: false,
+    },
+    7: {
+      name: "Test 7",
+      description: "ABC",
+      sentence: "ABC{}",
+      isOptionOpened: false,
+    },
   });
 
   // Function to toggle the isOptionOpened field for a specific key (e.g., "0" or "1")
@@ -30,41 +66,43 @@ function SentenceList({ currentSentence }) {
 
   return (
     <>
-      <div className="p-10 [&>*]:text-xs [&>*]:sm:text-base">
-        <div className=" w-full grid gap-1">
-          <div className="p-1">
-            <h5>Sentence List</h5>
-          </div>
-          <div>Current Sentence : ABCSDEF</div>
-          <div className="flex justify-center gap-2 p-1">
-            <button className="bg-amber-700 w-fit h-fit f p-1 rounded-1">
-              ⟳ Refresh
+      <div className="sm:p-10 p-2 [&>*]:text-xs [&>*]:sm:text-base">
+        <div className=" w-full flex flex-col gap-1">
+          <div className="p-1 flex w-full border-b h-full">
+            <div className="p-1 w-[80dvh]">
+              <h5>Sentence List</h5>
+            </div>
+            <button className="bg-amber-700 w-[10dvh] h-full p-1 rounded-1 sm:h-full">
+              ⟳
             </button>
+          </div>
+          {/* <div>Current Sentence : ABCSDEF</div> */}
+          <div className="flex justify-center gap-2 p-1">
             {/* <button className="bg-amber-900 w-fit h-fit f p-1 rounded-1">
               ⇓ Load
             </button> */}
           </div>
-          <div className="grid gap-1">
+          <div className="grid gap-1 max-h-[20dvh] overflow-y-scroll h-full  max-w-[100%]">
             {Object.keys(sentenceList).map((value, index) => (
               <div className="flex w-full gap-2">
                 <button
                   key={index}
-                  className="bg-yellow-800 hover:bg-yellow-600 w-[70%] rounded-1 "
+                  className="bg-yellow-800 hover:bg-yellow-600 w-full rounded-1 p-1 overflow-hidden"
                 >
                   {sentenceList[value].name} : {sentenceList[value].description}{" "}
                   | Sentence : {sentenceList[value].sentence}
                 </button>
+                <button className="bg-amber-900 w-fit h-full f p-1 rounded-1">
+                  ⇓ Load
+                </button>
 
                 {sentenceList[value].isOptionOpened ? (
                   <>
-                    <button className="bg-amber-900 w-[10%] h-fit f p-1 rounded-1">
-                      ⇓ Load
-                    </button>
-                    <button className="bg-red-800 hover:bg-red-600 place-self-end w-[10%] h-full rounded-1 ">
+                    <button className="bg-red-800 hover:bg-red-600 place-self-end w-full h-full rounded-1 p-1">
                       ✖ Delete
                     </button>
                     <button
-                      className="w-[10%] bg-gray-900"
+                      className="w-full bg-gray-900 p-1"
                       onClick={() => {
                         toggleOption(value);
                       }}
@@ -75,7 +113,7 @@ function SentenceList({ currentSentence }) {
                 ) : (
                   <>
                     <button
-                      className="w-[30%] bg-gray-900"
+                      className="w-[10%] bg-gray-900 p-1"
                       onClick={() => {
                         toggleOption(value);
                       }}
