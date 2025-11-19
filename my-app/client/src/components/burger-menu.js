be3db1f27132ab424d9884fad62b2d7397d4d9bb
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-19 23:35:17 +0900

    chore : Add Temporary Method to Hide the Tutorial when Logged In

--- my-app/client/src/components/burger-menu.js
+++ my-app/client/src/components/burger-menu.js
@@ -19,73 +19,77 @@ function BurgerMenu({
       animate={{ x: isOpen ? 0 : "100%" }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
     >
-      {/* Logo Sentence Repeater */}
-      <div className="flex flex-row items-center justify-center">
-        <img
-          src="./logo512.png"
-          alt="sentence-repeater-logo"
-          className="w-[10%] h-auto"
-        ></img>
-      </div>
-
-      {/* Help Sentence Repeater */}
-      <div className="w-full h-auto">
-        <motion.button
-          className="w-full h-auto py-2 border-b border-gray-200"
-          onClick={() => setIsTutorialOpen(!isTutorialOpen)}
-        >
-          <div className="w-full text-center flex justify-between items-center px-4">
-            <h4 className="w-full">How to Use</h4>
-            <motion.span
-              animate={{ rotate: isTutorialOpen ? 180 : 0 }}
-              transition={{ duration: 0.3 }}
-              className="text-lg text-opposite-color"
-            >
-              ▼
-            </motion.span>
+      {incomingCurrentUser ? (
+        <SentenceList
+          incomingLink={incomingLink}
+          incomingCurrentUser={incomingCurrentUser}
+        ></SentenceList>
+      ) : (
+        <>
+          {/* Logo Sentence Repeater */}
+          <div className="flex flex-row items-center justify-center">
+            <img
+              src="./logo512.png"
+              alt="sentence-repeater-logo"
+              className="w-[10%] h-auto"
+            ></img>
           </div>
-        </motion.button>
 
-        <motion.div
-          className="overflow-hidden"
-          initial={{ height: 0 }}
-          animate={{ height: isTutorialOpen ? "auto" : 0 }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-        >
-          <motion.div
-            className="p-5 [&>*]:text-sm border-b border-gray-200 lg:[&>*]:text-lg"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: isTutorialOpen ? 1 : 0 }}
-            transition={{ duration: 0.3, delay: isTutorialOpen ? 0.1 : 0 }}
-          >
-            <p>Welcome to Sentence Repeater!</p>
-            <p>Write your Sentence in the Text Input</p>
-            <p>Provide the Generate Amount</p>
-            <p>Press Generate!</p>
-            <strong className="text-amber-600">
-              <p>
-                Write "{"{}"}" to define a Variable in the Sentence inside the
-                Text Input
-              </p>
-            </strong>
-          </motion.div>
-        </motion.div>
-      </div>
+          {/* Help Sentence Repeater */}
+          <div className="w-full h-auto">
+            <motion.button
+              className="w-full h-auto py-2 border-b border-gray-200"
+              onClick={() => setIsTutorialOpen(!isTutorialOpen)}
+            >
+              <div className="w-full text-center flex justify-between items-center px-4">
+                <h4 className="w-full">How to Use</h4>
+                <motion.span
+                  animate={{ rotate: isTutorialOpen ? 180 : 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="text-lg text-opposite-color"
+                >
+                  ▼
+                </motion.span>
+              </div>
+            </motion.button>
 
-      {/* Dark Mode Repeater */}
-      <div className="w-full h-auto flex flex-row justify-center items-center text-center gap-2">
-        <div>Dark Mode |</div>
+            <motion.div
+              className="overflow-hidden"
+              initial={{ height: 0 }}
+              animate={{ height: isTutorialOpen ? "auto" : 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <motion.div
+                className="p-5 [&>*]:text-sm border-b border-gray-200 lg:[&>*]:text-lg"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isTutorialOpen ? 1 : 0 }}
+                transition={{ duration: 0.3, delay: isTutorialOpen ? 0.1 : 0 }}
+              >
+                <p>Welcome to Sentence Repeater!</p>
+                <p>Write your Sentence in the Text Input</p>
+                <p>Provide the Generate Amount</p>
+                <p>Press Generate!</p>
+                <strong className="text-amber-600">
+                  <p>
+                    Write "{"{}"}" to define a Variable in the Sentence inside
+                    the Text Input
+                  </p>
+                </strong>
+              </motion.div>
+            </motion.div>
+          </div>
 
-        <CustomToggle
-          onChange={darkModeChanges}
-          checked={isDarkMode}
-        ></CustomToggle>
-      </div>
+          {/* Dark Mode Repeater */}
+          <div className="w-full h-auto flex flex-row justify-center items-center text-center gap-2">
+            <div>Dark Mode |</div>
 
-      <SentenceList
-        incomingLink={incomingLink}
-        incomingCurrentUser={incomingCurrentUser}
-      ></SentenceList>
+            <CustomToggle
+              onChange={darkModeChanges}
+              checked={isDarkMode}
+            ></CustomToggle>
+          </div>
+        </>
+      )}
     </motion.div>
   );
 }
