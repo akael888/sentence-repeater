ef559eccde9909dd30618ee6dd6a3c31ea18f46b
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-11-22 11:52:45 +0900

    feat : UI Improvement and Animation for Register and Login Form

--- my-app/client/src/components/auth-actions.js
+++ my-app/client/src/components/auth-actions.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import Login from "./login-form";
 import Register from "./register-form";
+import { motion } from "motion/react";
 
 function AuthActions({
   incomingHandleCurrentUserChanges,
   incomingCurrentLink,
 }) {
   const [authMessage, setAuthMessage] = useState("");
+  const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
+  const [isRegisterFormOpen, setIsRegisterFormOpen] = useState(false);
 
   const handleAuthMessageChanges = (message) => {
     setAuthMessage(message);
+  };
+
+  const toggleIsLoginFormOpen = () => {
+    setIsLoginFormOpen(!isLoginFormOpen);
+    if (isRegisterFormOpen) {
+      setIsRegisterFormOpen(false);
+    }
+  };
+  const toggleIsRegisterFormOpen = () => {
+    setIsRegisterFormOpen(!isRegisterFormOpen);
+    if (setIsLoginFormOpen) {
+      setIsLoginFormOpen(false);
+    }
   };
 
   return (
@@ -17,20 +33,29 @@ function AuthActions({
       <div className="h-full items-center">
         <div className="flex justify-end items-center h-full gap-2">
           {authMessage ? (
-            <div className="flex items-center justify-center h-full text-red-100">
+            <motion.div
+              className="flex items-center justify-center h-full text-red-100"
+              initial={{ x: 10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
               {authMessage}
-            </div>
+            </motion.div>
           ) : null}
 
           <Register
             currentLink={incomingCurrentLink}
             incomingHandleCurrentUserChanges={incomingHandleCurrentUserChanges}
             incomingAuthMessageChanges={handleAuthMessageChanges}
+            incomingIsRegisterFormOpen={isRegisterFormOpen}
+            incomingToggleRegisterFormOpen={toggleIsRegisterFormOpen}
           ></Register>
           <Login
             currentLink={incomingCurrentLink}
             incomingHandleCurrentUserChanges={incomingHandleCurrentUserChanges}
             incomingAuthMessageChanges={handleAuthMessageChanges}
+            incomingIsLoginFormOpen={isLoginFormOpen}
+            incomingToggleLoginFormOpen={toggleIsLoginFormOpen}
           ></Login>
         </div>
       </div>
--- my-app/client/src/components/login-form.js
+++ my-app/client/src/components/login-form.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import { motion } from "motion/react";
 
 function Login({
   currentLink,
   incomingHandleCurrentUserChanges,
   incomingAuthMessageChanges,
+  incomingIsLoginFormOpen,
+  incomingToggleLoginFormOpen,
 }) {
-  const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
-
   const [loginData, setLoginData] = useState({ username: "", password: "" });
 
   const handleLoginDataChanges = (e) => {
@@ -37,24 +38,34 @@ function Login({
         incomingAuthMessageChanges(`Login Failed : ${data.msg}`);
       }
     } catch (error) {
+      incomingAuthMessageChanges(error.message);
       console.log(error);
     }
   };
 
   return (
     <>
-      <form className="w-fit h-full flex" onSubmit={handleSubmit}>
-        {isLoginFormOpen ? (
-          <div className="text-black w-full h-full border-black">
+      <motion.form className="w-fit h-full flex gap-1" onSubmit={handleSubmit}>
+        {incomingIsLoginFormOpen ? (
+          <motion.div
+            initial={{ opacity: 0, x: 10 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+            }}
+            exit={{ opacity: 0, x: 10 }}
+            className="text-black w-full h-full border-black flex gap-1"
+          >
             <input
-              className="max-w-[10dvh] h-full"
+              className="max-w-full h-full"
               name="username"
               placeholder="Username"
               value={loginData.username}
               onChange={handleLoginDataChanges}
             ></input>
             <input
-              className="max-w-[10dvh] h-full"
+              className="max-w-full h-full"
               name="password"
               placeholder="Password"
               type="password"
@@ -67,18 +78,18 @@ function Login({
             >
               Submit
             </button>
-          </div>
+          </motion.div>
         ) : null}
         <button
           className="border-1 rounded-1 p-1 hover:bg-white hover:text-black"
           type="button"
           onClick={() => {
-            setIsLoginFormOpen(!isLoginFormOpen);
+            incomingToggleLoginFormOpen();
           }}
         >
           Login
         </button>
-      </form>
+      </motion.form>
     </>
   );
 }
